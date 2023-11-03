74f149ae8429a23818751e4c09c70a70dd0e8e05
asiopta <128475295+asiopta@users.noreply.github.com>
2023-11-03 10:46:57 +0100

    Update Main.c

--- Main.c
+++ Main.c
@@ -7,10 +7,17 @@
 #include "TP3_amen.h"
 
 
-int main(int argc, char** argv)
+int main()
 {
-    int choix;
 
+    int choix;
+    float montant ;
+    float solde;
+    float total_transactions;
+    int idEtu_source, idEtu_destination, verif;
+    char descr[20];
+    char date_recherchee[9];
+    int idetu;
     char date_du_jour[9] = "20231107";
     BlockChain blockchain = NULL;
 
@@ -38,21 +45,25 @@ int main(int argc, char** argv)
         case 1:
             printf("Vous avez choisi l'option 1, qui correspond a l'ajout d'une transaction en tete d'une liste de transactions.\n");
             T_Block *premier_bloc = blockchain;
+
             if (premier_bloc != NULL)
             {
-                T_Transaction *transactions = premier_bloc->listeTransactions;
-                int idetu;
-                float montant;
-                char descr[20];
-
-
                 printf("\nVeuillez entrer l'id de l'etudiant: ");
                 scanf("%d", &idetu);
                 printf("\nVeuillez entrer le montant de la transaction: ");
                 scanf("%f", &montant);
                 printf("\nVeuillez entrer la description correspondant a la transaction: ");
-                scanf("%s", &descr);
-                ajouterTransaction(idetu, montant, &descr, transactions);
+                scanf("%s", descr);
+
+                // Ajout de la transaction � ajouter en t�te du premier bloc
+                premier_bloc->listeTransactions = ajouterTransaction(idetu, montant, descr, premier_bloc->listeTransactions);
+
+                // Affichage du r�sultat
+                printf("Le premier bloc devient alors: \n");
+                print_bloc(premier_bloc);
+
+                // Lib�ration de la m�moire pour la transaction
+                //libererTransaction(trans_ajoutee);
             }
             else
             {
@@ -64,25 +75,33 @@ int main(int argc, char** argv)
             break;
 
         case 2:
-            printf("Vous avez choisi l'option 2, qui croorespond a l'ajout d'un bloc en tete de la BlockChain.\n");
-            ajouterBlock(blockchain, &date_du_jour);
+            printf("Vous avez choisi l'option 2, qui correspond a l'ajout d'un bloc en tete de la BlockChain.\n");
+            // Ajout d'un bloc avec un dateBlock correspondant � la date suivante et un idBlock en autoincr�ment
+            blockchain = ajouterBlock(blockchain, date_suivante(date_du_jour));
+            // Affichage de la blockchain apr�s ajout du bloc
+            print_blockchain(blockchain);
             break;
 
         case 3:
-            int idEtu;
-            char date_recherchee[9];
-            float total_transactions;
 
-            printf("Vous avez choisi l'option 3, qui correspond au calcul de la somme des EATCoin credites et depenses par un etudiant sur une journee. \n");
+
+           printf("Vous avez choisi l'option 3, qui correspond au calcul de la somme des EATCoin credites et depenses par un etudiant sur une journee. \n");
             printf("\nVeuillez entrer l'id de l'etudiant en question: ");
-            scanf("%d", &idEtu);
+            scanf("%d", &idetu);
             printf("\nVeuillez entrer la date de la journee recherchee (en respectant le format de date AAAAMMJJ): ");
-            scanf("%s", &date_recherchee);
-            T_Block *bloc_recherche = rechercherBlocParDate(blockchain, &date_recherchee);
+            scanf("%s", date_recherchee);
+
+            printf("--------------\n") ;
+
+            printf("La date recherchee est %s.\n", date_recherchee);
+            printf("La blockchain est: \n");
+            print_blockchain(blockchain);
+            T_Block *bloc_recherche = rechercherBlocParDate(blockchain, date_recherchee);
+
             if (bloc_recherche != NULL) {
-                // On a trouvé le bloc correspondant à la date de recherche.
-                total_transactions = totalTransactionEtudiantBlock(idEtu, bloc_recherche);
-                printf("La somme des EATCoin credites et depenses par l'etudiant d'id %d sur la journee %s est de %f EATCoin.\n", idEtu, date_recherchee, total_transactions);
+                // On a trouv� le bloc correspondant � la date de recherche.
+                total_transactions = totalTransactionEtudiantBlock(idetu, *bloc_recherche);
+                printf("La somme des EATCoin credites et depenses par l'etudiant d'id %d sur la journee %s est de %f EATCoin.\n", idetu, date_recherchee, total_transactions);
             }
             else {
                 printf("Ooops! Aucun bloc avec la date recherchee n'a ete trouve.\n");
@@ -91,47 +110,43 @@ int main(int argc, char** argv)
             }
             break;
 
+
         case 4:
-            int idEtu;
-            float solde;
+
             printf("Vous avez choisi l'option 4, qui correspond au calcul du solde total d'un etudiant. \n");
-            printf("Veuillez enter l'id de l'etudiant en question: ");
-            scanf("%d", &idEtu);
-            solde = soldeEtudiant(idEtu, blockchain);
-            printf("\nLe solde total de cet etudiant d'id %d est de %f EATCoin.\n", idEtu, solde);
+            printf("Veuillez entrer l'id de l'etudiant en question: ");
+            scanf("%d", &idetu);
+            solde = soldeEtudiant(idetu, blockchain);
+            printf("\nLe solde total de cet etudiant d'id %d est de %f EATCoin.\n", idetu, solde);
             break;
 
         case 5:
-            int idEtu, verif; //verif ne sert à rien dans ce cas
-            float montant;
-            char descr[20];
             printf("Vous avez choisi l'option 5, qui correspond au rechargement du compte d'un etudiant. \n");
             printf("Veuillez entrer l'id de l'etudiant en question: ");
-            scanf("%d", &idEtu);
+            scanf("%d", &idetu);
             printf("\nVeuillez entrer le montant du rechargement: ");
             scanf("%f", &montant);
             printf("\nVeuillez entrer la description correspondant au rechargement (ici considere comme une transaction): ");
-            scanf("%s", &descr);
-            crediter(idEtu, montant, &descr, &date_du_jour ,blockchain);
+            scanf("%s", descr);
+            crediter(idetu, montant, descr, date_du_jour, blockchain);
+            printf("rechargement réussi");
             break;
 
         case 6:
-            int idEtu, verif = 1; //je ne vois pas prq on initialise verif avec 1, ca change pas grande chose mais.....
-            float montant;
-            char descr[20];
 
             printf("Vous avez choisi l'option 6, qui correspond au paiement d'un repas. \n");
             printf("Veuillez entrer l'id de l'etudiant en question: ");
-            scanf("%d", &idEtu);
+            scanf("%d", &idetu);
             printf("\nVeuillez entrer le montant du paiement: ");
             scanf("%f", &montant);
             printf("\nVeuillez entrer la description correspondant au paiement: ");
-            scanf("%s", &descr);
+            scanf("%s", descr);
 
-            verif = payer(idEtu, montant, &descr, &date_du_jour, blockchain);
-            if (verif)
+            verif = payer(idetu, montant, descr, date_du_jour, &blockchain);
+            if (verif == 0)
             {
-                printf("Oops! L'operation a echoue... Reessayez! Nous vous redirigeons vers le menu.\n");
+                printf("Ooops! Cet etudiant d'id %d n'a pas assez de EATCoin pour pouvoir effectuer le paiement.\n", idetu);
+                printf("Vous devez d'abord recharger son compte, en choisissant l'option 5. Nous vous redirigeons vers le menu.\n");
                 continue;
             }
             else
@@ -141,18 +156,15 @@ int main(int argc, char** argv)
             break;
 
         case 7:
-            int idEtu;
             printf("Vous avez choisi l'option 7, qui correspond a l'affichage de l'historique d'un etudiant. \n");
             printf("Veuillez entrer l'id de l'etudiant en question: ");
-            scanf("%d", &idEtu);
+            scanf("%d", &idetu);
 
-            consulter(idEtu, blockchain);
+            consulter(idetu, blockchain);
             break;
 
         case 8:
-            int idEtu_source, idEtu_destination, verif;
-            float montant;
-            char descr[20];
+
             printf("Vous avez choisi l'option 8, qui correspond au transfert de EATCoins entre deux etudiants. \n");
             printf("Veuillez entrer l'id de l'etudiant qui souhaite effectuer le transfert: ");
             scanf("%d", &idEtu_source);
@@ -161,43 +173,34 @@ int main(int argc, char** argv)
             printf("\nVeuillez entrer le montant du transfert: ");
             scanf("%f", &montant);
             printf("\nVeuillez entrer la description correspondant au transfert: ");
-            scanf("%s", &descr);
-            verif = transfert(idEtu_source, idEtu_destination, montant, &descr, &date_du_jour, blockchain);
-            if (verif)
+            scanf("%s", descr);
+            verif = transfert(idEtu_source, idEtu_destination, montant, descr, date_du_jour, blockchain);
+            if (verif == 0)
             {
-                printf("Oops! L'operation de transfert a echoue. \n");
-                printf("Ressayer! Nous vous redirigeons vers le menu.\n");
+                printf("Oops! L'operation de transfert a echoue, car l'etudiant d'id %d, qui souhaite effectuer un transfert, n'a pas assez de EATCoin dans son compte. \n", idEtu_source);
+                printf("Vous pouvez recharger son compte, en choisissant l'option 5. Nous vous redirigeons vers le menu.\n");
                 continue;
-            }
-            else
+            }else{
                 printf("Le transfert a ete effectue avec succes!\n");
+                print_blockchain(blockchain);
+            }
             break;
 
         case 9:
             printf("Au revoir!");
+            // Lib�rer la m�moire avant de sortir
+            libererBlockchain(blockchain);
             exit(0); // Quitter le programme
 
         default:
             printf("Vous avez entre un numero invalide. Recommencez!\n");
-            // Puis, on réaffiche le menu et on redemande la sélection d'une option en cas de choix invalide
+            // Puis, on r�affiche le menu et on redemande la s�lection d'une option en cas de choix invalide
         }
     }
+    viderBuffer(); // supprimer de l'entree standard le retour charriot et les eventuels caracteres supplementaires tapes par l'utilisateur
 
-    /*char* source = "Bonjour";
-    char destination[20];
-    strcpy(destination, source);
-
-    printf("Source : %s\n", source);
-    printf("Destination : %s\n", destination);
-
-    T_Transaction* a = ajouterTransaction(1 , 12.5 , "ahv",NULL) ;
-    ajouterTransaction(1 , 23, "Baguette",a) ;
-    ajouterTransaction(1 , 23, "Baguette",a) ;
-    afficherTransaction(a) ;
-
-   printf("abc \n") ; */
-
-
+    // Lib�rer la m�moire
+    libererBlockchain(blockchain);
 
     return 0;
 }
